0580c4c61f2eaeb097c707190bb4bf60ca9256e7
jinzza456 <jinzza456@gmail.com>
2021-10-12 10:22:15 +0900

    for

---
+++ for.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// i := 0
+	// for i < 10 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// fmt.Println("최종 i 값은:", i)
+
+	var i int
+	for i = 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("최종 i 값은:", i)
+}
