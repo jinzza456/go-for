a52a587d754e574bff3c9b51b81f42da1bf93496
jinzza456 <jinzza456@gmail.com>
2021-10-12 10:58:06 +0900

    break-continue

--- for.go
+++ for.go
@@ -3,23 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// i := 0
-	// for i < 10 {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
-
-	// fmt.Println("최종 i 값은:", i)
-
-	// var i int
-	// for i = 0; i < 10; i++ {
-	// 	fmt.Println(i)
-	// }
-	// fmt.Println("최종 i 값은:", i)
-
 	var i int
 	for {
 		i++
+		if i == 5 {
+			continue
+		}
+		if i == 10 {
+			break
+		}
 		fmt.Println(i)
 	}
 	fmt.Println("최종 i 값은:", i)
